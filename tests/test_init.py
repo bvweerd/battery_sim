06d1966a0107d13727dbbb119497494eb85f79a8
bvweerd <bastiaan.vanweerd@aimvalley.com>
2025-04-22 10:50:00 +0200

    Install test framework

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,28 +1,9 @@
-import pytest
-from pytest_homeassistant_custom_component.common import MockConfigEntry
+"""Test component setup."""
+from homeassistant.setup import async_setup_component
 
-from custom_components.battery_sim.const import DOMAIN
+from custom_components.{{ cookiecutter.domain }}.const import DOMAIN
 
 
-@pytest.mark.asyncio
-async def test_async_setup_entry(hass):
-    """Test setting up the battery_sim integration."""
-
-    # Maak een nep config entry aan
-    config_entry = MockConfigEntry(
-        domain=DOMAIN,
-        title="Battery Sim Test",
-        data={"name": "TestBattery"},
-        unique_id="test_battery_id",
-    )
-
-    # Voeg deze toe aan Home Assistant
-    config_entry.add_to_hass(hass)
-
-    # Roep setup aan
-    assert await hass.config_entries.async_setup(config_entry.entry_id)
-    await hass.async_block_till_done()
-
-    # Controleer of de integratie correct in hass.data staat
-    assert DOMAIN in hass.data
-    assert config_entry.entry_id in hass.data[DOMAIN]
+async def test_async_setup(hass):
+    """Test the component gets setup."""
+    assert await async_setup_component(hass, DOMAIN, {}) is True
