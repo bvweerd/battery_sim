1a1f7badc61c3113180cdd4484957835b3a46e08
bvweerd <bastiaan.vanweerd@aimvalley.com>
2025-04-22 15:39:29 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,17 @@
 
 import pytest
 
+class DummyHandle:
+    def __init__(self, name):
+        self._name = name
+        self._max_charge_rate = 3.5
+        self._max_discharge_rate = 4.0
+        self._slider_limits = {}
+
+    def set_slider_limit(self, value, key):
+        self._slider_limits[key] = value
+
+
 @pytest.fixture
 def mock_handle():
     class DummyHandle:
