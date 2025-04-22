65dc2c896c4b0b4c06cecc98d7d356a54f0a9d1a
bvweerd <bastiaan.vanweerd@aimvalley.com>
2025-04-22 15:46:02 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,3 +23,7 @@ def mock_handle():
             self._charge_state = 5
     return DummyHandle()
 
+@pytest.fixture(autouse=True)
+def auto_enable_custom_integrations(enable_custom_integrations):
+    """Enable custom integrations."""
+    return
